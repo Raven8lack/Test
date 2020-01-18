dfb2b10a5fa506c5a60550002f24af3fbfb3e657
Evgeniy <logik34999@mail.ru>
2020-01-18 18:13:52 +0500

    tets"
    gdf
    "
    fdg"

---
+++ Assets/Script/ButtonScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class ButtonScript : MonoBehaviour
+{
+    public GameObject obj;
+    public Button bt;
+    public Color cl;
+
+    private string data;
+
+    private void Awake()
+    {
+        data = File.ReadAllText("Save/" + gameObject.GetInstanceID() + ".txt");
+        JsonUtility.FromJsonOverwrite(data, this);
+        SetInfo();
+    }
+
+    private void Update()
+    {
+        if(obj.GetComponent<Renderer>().material.color == Color.white)
+        {
+            bt.GetComponentInChildren<Text>().text = "White";
+        }
+        else
+        {
+            bt.GetComponentInChildren<Text>().text = "Blue";
+        }
+    }
+
+    public void OnButtonDown()
+    {
+        if (obj.GetComponent<Renderer>().material.color != Color.blue)
+        {
+            obj.GetComponent<Renderer>().material.color = Color.blue;
+        }
+        else
+        {
+            obj.GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+
+    private void CollectInfo()
+    {
+        cl = obj.GetComponent<Renderer>().material.color;
+    }
+
+    private void SetInfo()
+    {
+        obj.GetComponent<Renderer>().material.color = cl;
+    }
+
+    private void OnApplicationQuit()
+    {
+        CollectInfo();
+        data = JsonUtility.ToJson(this);
+        File.WriteAllText("Save/" + gameObject.GetInstanceID() + ".txt", data);
+    }
+}
---
+++ Assets/Script/CubeScrint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScrint : MonoBehaviour
+{
+    public static CubeScrint current;
+
+    private void OnMouseDown()
+    {
+        if(GetComponent<Renderer>().material.color != Color.blue)
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+        else
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+        
+    }
+}
